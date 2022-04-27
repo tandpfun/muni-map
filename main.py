2cd299a510e16d9937065850f190f09b3e3e0b5d
Coding <28990589+tandpfun@users.noreply.github.com>
2022-04-27 09:51:20 -0700

    train predictions

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+from requests import get
+import json
+
+route = 'KT'
+url='https://retro.umoiq.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=' + route
+
+while True:
+  positionData = get(url).json()
+  vehicles = positionData['vehicle']
+
+  for train in vehicles:
+    if train['predictable'] == 'true':
+      print('OWO NEW TRAIN')
+      print(train['routeTag'] + ': ' + train['lon'] + ' ' + train['lat'])
+  time.sleep(9)
