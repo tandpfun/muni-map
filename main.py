033133870d46e5f155d1f3f6cd21425911705f44
Coding <28990589+tandpfun@users.noreply.github.com>
2022-05-06 14:17:02 -0700

    better train location predictions

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import time
 from requests import get
 import json
+from math import radians, cos, sin, asin, sqrt
+
+def dist(lat1, long1, lat2, long2):
+    """
+    Replicating the same formula as mentioned in Wiki
+    """
+    # convert decimal degrees to radians 
+    lat1, long1, lat2, long2 = map(radians, [lat1, long1, lat2, long2])
+    # haversine formula 
+    dlon = long2 - long1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    # Radius of earth in kilometers is 6371
+    km = 6371* c
+    return km 
+
+pixel_locations = {
+  0: (37.755265, -122.462680),
+  1: (37.787849, -122.403262)
+}
 
 route = 'KT'
 url='https://retro.umoiq.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=' + route
@@ -11,5 +32,12 @@ while True:
 
   for train in vehicles:
     if train['predictable'] == 'true':
-      print(train['routeTag'] + ': ' + train['lon'] + ' ' + train['lat'])
+
+      distances = {}
+      for key in pixel_locations:
+        pixel = pixel_locations[key]
+        distances[key] = dist(float(train['lat']), float(train['lon']), pixel[0], pixel[1])
+
+      closest_light = sorted(distances.items(), key=lambda x: x[1])[0][0]
+      print('KT ' + train['lat'] + ', ' + train['lon'] + ' => ' + str(closest_light))
   time.sleep(9)
