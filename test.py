6d3dc37dab72dbd36f3bf6ce2b32be9acbf4c734
Coding <28990589+tandpfun@users.noreply.github.com>
2022-05-06 13:40:41 -0700

    c

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import neopixel
 pixel_pin = board.D21
 num_pixels = 60
 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=True, pixel_order=neopixel.GRBW)
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=neopixel.GRBW)
 
 i=0
 while True:
   i = i + 1
   pixels[i % 60] = (0, 0, 255)
   pixels[(i - 2) % 60] = (0, 0, 0)
+  pixels.show()
   time.sleep(.10)
