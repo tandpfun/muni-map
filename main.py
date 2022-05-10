668d48cbdc4b17c2d93a5601348194311bddefe8
Coding <28990589+tandpfun@users.noreply.github.com>
2022-05-10 10:34:01 -0700

    starting animation

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ num_pixels = 60
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=neopixel.GRBW)
 
+pixels.fill((0, 0, 255))
+
+time.sleep(5)
+
+pixels.fill(0)
+
 def dist(lat1, long1, lat2, long2):
     """
     Replicating the same formula as mentioned in Wiki
